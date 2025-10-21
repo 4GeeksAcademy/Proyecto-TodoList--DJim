702c17f5e59ab87a49fcf1ca19e0f134290c0e78
David Jimenez <226816431+dajim2210@users.noreply.github.com>
2025-10-21 04:28:02 +0000

    Faltan agregar funciones y estilos

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+let [tarea, setTarea] = useState("")
+let [lista, setLista] = useState([])
+
+
+const escribirTarea = (event) =>{
+setTarea (event.target.value)
+}
+
+const agregarTarea = (event) =>{
+   if (event.key==="Enter"){
+	setLista ([...lista, tarea])
+    setTarea ("")
+   }}
+
+   const eliminarTarea = (posicion) =>{
+	setLista (lista.filter((item, index)=>index!==posicion))
+   }
+	
+   return (
+		<div >
+			      
+			<div className="container">
+				<h1 className="text-center"> TodoList </h1>    
+			    <input type="text" placeholder="Whats needs to be done?" onChange={escribirTarea} value={tarea} onKeyDown={agregarTarea}/>
+			
+			<ul className ="list-unstyled">
+			
+			{lista.map((item, index)=>(<li className="list-group-item" key={index}>{item}<span onClick={()=>eliminarTarea(index)}>  ❌ </span></li>))}
+			
+			
+			
+			
+			</ul>
+			
+			<p className="paper">{lista.length} item left</p>
+			</div>
+
+			
 		</div>
 	);
 };
